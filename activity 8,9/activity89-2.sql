4ed71e836373f2ad1998d129a95cca3e4aa8eb7d
HubGitterHere <schmidj4@oregonstate.edu>
2025-11-03 22:15:10 -0800

    Delete activity 8,9 directory

--- activity 8,9/activity89-2.sql
+++
@@ -1,5 +0,0 @@
-SELECT InvoiceDetails.InvoiceID, Products.ProductName, InvoiceDetails.UnitPrice
-    FROM InvoiceDetails
-    INNER JOIN Products ON InvoiceDetails.ProductNumber = Products.ProductNumber
-    WHERE InvoiceID = 3
-    ORDER BY InvoiceDetails.UnitPrice ASC;
--- activity 8,9/activity89-5.sql
+++
@@ -1,5 +0,0 @@
-SELECT Products.ProductNumber, Products.ProductName, InvoiceDetails.LineTotal, 
-    InvoiceDetails.OrderQty, InvoiceDetails.UnitPrice
-    FROM InvoiceDetails
-    INNER JOIN Products ON InvoiceDetails.ProductNumber = Products.ProductNumber
-    WHERE InvoiceDetails.InvoiceID = 3;
